6a69581c850eed8b1f32dcc58d78b33c9caad996
CarlosGauret <carlosgauret@gmail.com>
2024-06-28 18:04:25 -0500

    Initial Commit

---
+++ PracticaCollectionView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaCollectionView2
+//
+//  Created by Liee Marie on 28/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
